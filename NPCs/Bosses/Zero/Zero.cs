61768ebda1e4dd3f889c60924ce934b2c9cdb1cb
Jon Oakeson <38961891+Alphakip@users.noreply.github.com>
2018-09-22 18:49:12 -0700

    compression of zero code - liz

--- NPCs/Bosses/Zero/Zero.cs
+++ NPCs/Bosses/Zero/Zero.cs
@@ -7,56 +7,31 @@ using Terraria.ID;
 using Terraria.ModLoader;
 
 namespace AAMod.NPCs.Bosses.Zero
-
 {
-
     [AutoloadBossHead]
-
     public class Zero : ModNPC
-
     {
-        
-
         private Player player;
-
         private float speed;
-
         public override void SetStaticDefaults()
-
         {
-
             DisplayName.SetDefault("Zero");
-
             Main.npcFrameCount[npc.type] = 3; 
-
         }
+
         public override void SetDefaults()
-
         {
-
             npc.width = 206;
-
             npc.height = 208;
-
             npc.aiStyle = 0;
-
-            npc.damage = 130;  //boss damage
-
-            npc.defense = 70;    //boss defense
-
+            npc.damage = 130;
+            npc.defense = 70;
             npc.lifeMax = 200000;
-
             npc.HitSound = SoundID.NPCHit4;
-
             npc.DeathSound = SoundID.NPCHit4;
-
             npc.noGravity = true;
-
             music = mod.GetSoundSlot(Terraria.ModLoader.SoundType.Music, "Sounds/Music/Zero");
-
             npc.noTileCollide = true;
-
-
             if (Main.expertMode)
             {
                 npc.value = 0;
@@ -65,22 +40,13 @@ namespace AAMod.NPCs.Bosses.Zero
             {
                 npc.value = 120000f;
             }
-
             npc.knockBackResist = -1f;
-
             npc.boss = true;
-
             npc.friendly = false;
-
             animationType = NPCID.SkeletronPrime;
-
             npc.npcSlots = 1f;
-
             npc.lavaImmune = true;
-
             npc.netAlways = true;
-
-            
         }
 
         public override bool? DrawHealthBar(byte hbPosition, ref float scale, ref Vector2 position)
@@ -90,52 +56,30 @@ namespace AAMod.NPCs.Bosses.Zero
         }
 
         public override void HitEffect(int hitDirection, double damage)
-
         {
-
             if (npc.life <= 0 && npc.type == mod.NPCType<Zero>())
-
             {
-
                 Gore.NewGore(npc.position, npc.velocity, mod.GetGoreSlot("Gore/ZeroGore1"), 1f);
-
                 Gore.NewGore(npc.position, npc.velocity, mod.GetGoreSlot("Gore/ZeroGore1"), 1f);
-
                 Gore.NewGore(npc.position, npc.velocity, mod.GetGoreSlot("Gore/ZeroGore1"), 1f);
-
                 Gore.NewGore(npc.position, npc.velocity, mod.GetGoreSlot("Gore/ZeroGore1"), 1f);
-
                 Gore.NewGore(npc.position, npc.velocity, mod.GetGoreSlot("Gore/ZeroGore2"), 1f);
-
                 Gore.NewGore(npc.position, npc.velocity, mod.GetGoreSlot("Gore/ZeroGore3"), 1f);
-
                 Gore.NewGore(npc.position, npc.velocity, mod.GetGoreSlot("Gore/ZeroGore3"), 1f);
-
                 Gore.NewGore(npc.position, npc.velocity, mod.GetGoreSlot("Gore/ZeroGore3"), 1f);
-
                 Gore.NewGore(npc.position, npc.velocity, mod.GetGoreSlot("Gore/ZeroGore3"), 1f);
-
                 npc.position.X = npc.position.X + npc.width / 2;
-
                 npc.position.Y = npc.position.Y + npc.height / 2;
-
                 npc.width = 100;
-
                 npc.height = 100;
-
                 npc.position.X = npc.position.X - npc.width / 2;
-
                 npc.position.Y = npc.position.Y - npc.height / 2;
-
                 Vector2 spawnAt = npc.Center + new Vector2(0f, npc.height / 2f);
-
-                
                 if (Main.expertMode)
                 {
                     NPC.NewNPC((int)spawnAt.X, (int)spawnAt.Y, mod.NPCType("ZeroAwakened"));
                 }
             }
-
         }
 
         public override void NPCLoot()
@@ -146,14 +90,11 @@ namespace AAMod.NPCs.Bosses.Zero
             }
             else
             {
-
                 npc.DropLoot(mod.ItemType("ApocalyptitePlate"), 20, 30);
                 npc.DropLoot(mod.ItemType("UnstableSingularity"), 25, 35);
-
                 string[] lootTable = { "RiftShredder", "EventHorizon", "VoidStar", "RealityCannon", "TeslaHand", "ZeroStar" };
                 int loot = Main.rand.Next(lootTable.Length);
                 npc.DropLoot(mod.ItemType(lootTable[loot]));
-
                 npc.DropLoot(Items.Vanity.Mask.ZeroMask.type, 1f / 7);
                 npc.DropLoot(Items.Blocks.ZeroTrophy.type, 1f / 10);
             }
@@ -166,7 +107,6 @@ namespace AAMod.NPCs.Bosses.Zero
                 potionType = ItemID.GreaterHealingPotion;   //boss drops
                 AAWorld.downedZero = true;
             }
-            
         }
 
         public override void PostDraw(SpriteBatch spriteBatch, Color drawColor)
@@ -232,350 +172,189 @@ namespace AAMod.NPCs.Bosses.Zero
         }
 
         public override void ScaleExpertStats(int numPlayers, float bossLifeScale)
-
         {
-
-            npc.lifeMax = (int)(npc.lifeMax * 0.8f * bossLifeScale);  //boss life scale in expertmode
-
-            npc.damage = (int)(npc.damage * 0.7f);  //boss damage increase in expermode
-
+            npc.lifeMax = (int)(npc.lifeMax * 0.8f * bossLifeScale);
+            npc.damage = (int)(npc.damage * 0.7f);
         }
 
 
 
         public override void AI()
-
         {
-
             npc.damage = npc.defDamage;
-
             npc.defense = npc.defDefense;
-
             if (npc.ai[0] == 0f && Main.netMode != 1)
-
             {
-
                 npc.TargetClosest(true);
-
                 npc.ai[0] = 1f;
-
                 int num440 = NPC.NewNPC((int)(npc.position.X + (float)(npc.width / 2)), (int)npc.position.Y + npc.height / 2, mod.NPCType("RiftShredder"), npc.whoAmI, 0f, 0f, 0f, 0f, 255);
-
-
                 Main.npc[num440].ai[0] = -1f;
-
                 Main.npc[num440].ai[1] = npc.whoAmI;
-
                 Main.npc[num440].target = npc.target;
-
                 Main.npc[num440].netUpdate = true;
-
                 num440 = NPC.NewNPC((int)(npc.position.X + npc.width / 2), (int)npc.position.Y + npc.height / 2, mod.NPCType("TeslaHand"), npc.whoAmI, 0f, 0f, 0f, 0f, 255);
-
                 Main.npc[num440].ai[0] = 1f;
-
                 Main.npc[num440].ai[1] = npc.whoAmI;
-
                 Main.npc[num440].target = npc.target;
-
                 Main.npc[num440].netUpdate = true;
-
                 num440 = NPC.NewNPC((int)(npc.position.X + npc.width / 2), (int)npc.position.Y + npc.height / 2, mod.NPCType("VoidStar"), npc.whoAmI, 0f, 0f, 0f, 0f, 255);
-
                 Main.npc[num440].ai[0] = -1f;
-
                 Main.npc[num440].ai[1] = npc.whoAmI;
-
                 Main.npc[num440].target = npc.target;
-
                 Main.npc[num440].ai[3] = 150f;
-
                 Main.npc[num440].netUpdate = true;
-
                 num440 = NPC.NewNPC((int)(npc.position.X + npc.width / 2), (int)npc.position.Y + npc.height / 2, mod.NPCType("RealityCannon"), npc.whoAmI, 0f, 0f, 0f, 0f, 255);
-
                 Main.npc[num440].ai[0] = 1f;
-
                 Main.npc[num440].ai[1] = npc.whoAmI;
-
                 Main.npc[num440].target = npc.target;
-
                 Main.npc[num440].netUpdate = true;
-
                 Main.npc[num440].ai[3] = 150f;
-
             }
 
             if (Main.player[npc.target].dead || Math.Abs(npc.position.X - Main.player[npc.target].position.X) > 6000f || Math.Abs(npc.position.Y - Main.player[npc.target].position.Y) > 6000f)
-
             {
-
                 npc.TargetClosest(true);
-
                 if (Main.player[npc.target].dead || Math.Abs(npc.position.X - Main.player[npc.target].position.X) > 6000f || Math.Abs(npc.position.Y - Main.player[npc.target].position.Y) > 6000f)
-
                 {
-
                     npc.ai[1] = 3f;
-
                 }
-
             }
 
             if (npc.ai[1] == 0f)
-
             {
-
                 npc.ai[2] += 1f;
-
                 if (npc.ai[2] >= 600f)
-
                 {
-
                     npc.ai[2] = 0f;
-
                     npc.ai[1] = 1f;
-
                     npc.TargetClosest(true);
-
                     npc.netUpdate = true;
-
                 }
 
                 npc.rotation = npc.velocity.X / 15f;
-
                 if (npc.position.Y > Main.player[npc.target].position.Y - 200f)
-
                 {
-
                     if (npc.velocity.Y > 0f)
-
                     {
-
                         npc.velocity.Y = npc.velocity.Y * 0.98f;
-
                     }
-
                     npc.velocity.Y = npc.velocity.Y - 0.1f;
-
                     if (npc.velocity.Y > 2f)
-
                     {
-
                         npc.velocity.Y = 2f;
-
                     }
-
                 }
 
                 else if (npc.position.Y < Main.player[npc.target].position.Y - 500f)
-
                 {
-
                     if (npc.velocity.Y < 0f)
-
                     {
-
                         npc.velocity.Y = npc.velocity.Y * 0.98f;
-
                     }
-
                     npc.velocity.Y = npc.velocity.Y + 0.1f;
-
                     if (npc.velocity.Y < -2f)
-
                     {
-
                         npc.velocity.Y = -2f;
-
                     }
-
                 }
 
                 if (npc.position.X + npc.width / 2 > Main.player[npc.target].position.X + Main.player[npc.target].width / 2 + 100f)
-
                 {
-
                     if (npc.velocity.X > 0f)
-
                     {
-
                         npc.velocity.X = npc.velocity.X * 0.98f;
-
                     }
-
                     npc.velocity.X = npc.velocity.X - 0.1f;
-
                     if (npc.velocity.X > 8f)
-
                     {
-
                         npc.velocity.X = 8f;
-
                     }
-
                 }
 
                 if (npc.position.X + npc.width / 2 < Main.player[npc.target].position.X + Main.player[npc.target].width / 2 - 100f)
-
                 {
-
                     if (npc.velocity.X < 0f)
-
                     {
-
                         npc.velocity.X = npc.velocity.X * 0.98f;
-
                     }
-
                     npc.velocity.X = npc.velocity.X + 0.1f;
-
                     if (npc.velocity.X < -8f)
-
                     {
-
                         npc.velocity.X = -8f;
-
                         return;
-
                     }
-
                 }
-
             }
 
             else
-
             {
-
                 if (npc.ai[1] == 1f)
-
                 {
-
                     npc.defense *= 2;
-
                     npc.damage *= 2;
-
                     npc.ai[2] += 1f;
-
                     if (npc.ai[2] == 2f)
-
                     {
-
                         Main.PlaySound(15, (int)npc.position.X, (int)npc.position.Y, 0, 1f, 0f);
-
                     }
 
                     if (npc.ai[2] >= 400f)
-
                     {
-
                         npc.ai[2] = 0f;
-
                         npc.ai[1] = 0f;
-
                     }
-
                     npc.rotation += npc.direction * 0.3f;
-
                     Vector2 vector44 = new Vector2(npc.position.X + npc.width * 0.5f, npc.position.Y + npc.height * 0.5f);
-
                     float num441 = Main.player[npc.target].position.X + Main.player[npc.target].width / 2 - vector44.X;
-
                     float num442 = Main.player[npc.target].position.Y + Main.player[npc.target].height / 2 - vector44.Y;
-
                     float num443 = (float)Math.Sqrt(num441 * num441 + num442 * num442);
-
                     num443 = 2f / num443;
-
                     npc.velocity.X = num441 * num443;
-
                     npc.velocity.Y = num442 * num443;
-
                     return;
-
                 }
 
                 if (npc.ai[1] == 2f)
-
                 {
-
                     npc.damage = 1000;
-
                     npc.defense = 9999;
-
                     npc.rotation += npc.direction * 0.3f;
-
                     Vector2 vector45 = new Vector2(npc.position.X + npc.width * 0.5f, npc.position.Y + npc.height * 0.5f);
-
                     float num444 = Main.player[npc.target].position.X + Main.player[npc.target].width / 2 - vector45.X;
-
                     float num445 = Main.player[npc.target].position.Y + Main.player[npc.target].height / 2 - vector45.Y;
-
                     float num446 = (float)Math.Sqrt(num444 * num444 + num445 * num445);
-
                     float num447 = 10f;
-
                     num447 += num446 / 100f;
-
                     if (num447 < 8f)
-
                     {
-
                         num447 = 8f;
-
                     }
-
                     if (num447 > 32f)
-
                     {
-
                         num447 = 32f;
-
                     }
-
                     num446 = num447 / num446;
-
                     npc.velocity.X = num444 * num446;
-
                     npc.velocity.Y = num445 * num446;
-
                     return;
-
                 }
 
                 if (npc.ai[1] == 3f)
-
                 {
-
                     npc.velocity.Y = npc.velocity.Y + 0.1f;
-
                     if (npc.velocity.Y < 0f)
-
                     {
-
                         npc.velocity.Y = npc.velocity.Y * 0.95f;
-
                     }
-
                     npc.velocity.X = npc.velocity.X * 0.95f;
-
                     if (npc.timeLeft > 500)
-
                     {
-
                         npc.timeLeft = 500;
-
                         return;
-
                     }
-
                 }
-
             }
-
         }
-
     }
 
     public class RealityCannon : ModNPC
