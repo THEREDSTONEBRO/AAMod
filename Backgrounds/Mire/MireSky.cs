7ae6b4bf96ddff2a5cde9a3c4add1b7be408d5e1
Mep Pep <Mitchell_Pepe@yahoo.com>
2018-09-23 18:51:14 -0400

    inferno/mire sky finish
    yay

--- Backgrounds/Mire/MireSky.cs
+++ Backgrounds/Mire/MireSky.cs
@@ -23,10 +23,6 @@ namespace AAMod.Backgrounds.Mire
 
         public override void Update(GameTime gameTime)
         {
-            Eggroll = Math.Abs(Main.GameUpdateCount) / 5f;
-            Pie = 1f * (float)Math.Sin(Eggroll);
-            color = Color.Lerp(Color.DodgerBlue, Color.Navy, Pie);
-
             if (Active)
             {
                 Intensity = Math.Min(1f, 0.01f + Intensity);
@@ -43,22 +39,34 @@ namespace AAMod.Backgrounds.Mire
             return new Color(Vector4.Lerp(value, Vector4.One, Intensity * 0.5f));
         }
 
-        public static float Eggroll;
-        public static float Pie;
-        public static Color color;
-
         public override void Draw(SpriteBatch spriteBatch, float minDepth, float maxDepth)
         {
             if (maxDepth >= 3.40282347E+38f && minDepth < 3.40282347E+38f)
             {
                 if (!Main.dayTime)
                 {
-                    if (color != null)
-                    {
-                        spriteBatch.Draw(Main.blackTileTexture, new Rectangle(0, 0, Main.screenWidth, Main.screenHeight), Color.Indigo * this.Intensity);
-                        var planetPos = new Vector2((Main.screenWidth / 4) * 3, Main.screenHeight / 4);
-                        spriteBatch.Draw(PlanetTexture, planetPos, null, Color.White * 0.9f * this.Intensity, 0f, new Vector2(PlanetTexture.Width >> 1, PlanetTexture.Height >> 1), 1f, SpriteEffects.None, 1f);
-                    }
+                    spriteBatch.Draw(Main.blackTileTexture, new Rectangle(0, 0, Main.screenWidth, Main.screenHeight), Color.Black * Intensity);
+                    spriteBatch.Draw(Main.blackTileTexture, new Rectangle(0, 0, Main.screenWidth, (Main.screenHeight / 20) * 1), Color.Indigo * 0.05f * Intensity);
+                    spriteBatch.Draw(Main.blackTileTexture, new Rectangle(0, 0, Main.screenWidth, (Main.screenHeight / 20) * 2), Color.Indigo * 0.05f * Intensity);
+                    spriteBatch.Draw(Main.blackTileTexture, new Rectangle(0, 0, Main.screenWidth, (Main.screenHeight / 20) * 3), Color.Indigo * 0.05f * Intensity);
+                    spriteBatch.Draw(Main.blackTileTexture, new Rectangle(0, 0, Main.screenWidth, (Main.screenHeight / 20) * 4), Color.Indigo * 0.05f * Intensity);
+                    spriteBatch.Draw(Main.blackTileTexture, new Rectangle(0, 0, Main.screenWidth, (Main.screenHeight / 20) * 5), Color.Indigo * 0.05f * Intensity);
+                    spriteBatch.Draw(Main.blackTileTexture, new Rectangle(0, 0, Main.screenWidth, (Main.screenHeight / 20) * 6), Color.Indigo * 0.05f * Intensity);
+                    spriteBatch.Draw(Main.blackTileTexture, new Rectangle(0, 0, Main.screenWidth, (Main.screenHeight / 20) * 7), Color.Indigo * 0.05f * Intensity);
+                    spriteBatch.Draw(Main.blackTileTexture, new Rectangle(0, 0, Main.screenWidth, (Main.screenHeight / 20) * 8), Color.Indigo * 0.05f * Intensity);
+                    spriteBatch.Draw(Main.blackTileTexture, new Rectangle(0, 0, Main.screenWidth, (Main.screenHeight / 20) * 9), Color.Indigo * 0.05f * Intensity);
+                    spriteBatch.Draw(Main.blackTileTexture, new Rectangle(0, 0, Main.screenWidth, (Main.screenHeight / 20) * 10), Color.Indigo * 0.05f * Intensity);
+                    spriteBatch.Draw(Main.blackTileTexture, new Rectangle(0, 0, Main.screenWidth, (Main.screenHeight / 20) * 11), Color.Indigo * 0.05f * Intensity);
+                    spriteBatch.Draw(Main.blackTileTexture, new Rectangle(0, 0, Main.screenWidth, (Main.screenHeight / 20) * 12), Color.Indigo * 0.05f * Intensity);
+                    spriteBatch.Draw(Main.blackTileTexture, new Rectangle(0, 0, Main.screenWidth, (Main.screenHeight / 20) * 13), Color.Indigo * 0.05f * Intensity);
+                    spriteBatch.Draw(Main.blackTileTexture, new Rectangle(0, 0, Main.screenWidth, (Main.screenHeight / 20) * 14), Color.Indigo * 0.05f * Intensity);
+                    spriteBatch.Draw(Main.blackTileTexture, new Rectangle(0, 0, Main.screenWidth, (Main.screenHeight / 20) * 15), Color.Indigo * 0.05f * Intensity);
+                    spriteBatch.Draw(Main.blackTileTexture, new Rectangle(0, 0, Main.screenWidth, (Main.screenHeight / 20) * 16), Color.Indigo * 0.05f * Intensity);
+                    spriteBatch.Draw(Main.blackTileTexture, new Rectangle(0, 0, Main.screenWidth, (Main.screenHeight / 20) * 17), Color.Indigo * 0.05f * Intensity);
+                    spriteBatch.Draw(Main.blackTileTexture, new Rectangle(0, 0, Main.screenWidth, (Main.screenHeight / 20) * 18), Color.Indigo * 0.05f * Intensity);
+                    spriteBatch.Draw(Main.blackTileTexture, new Rectangle(0, 0, Main.screenWidth, (Main.screenHeight / 20) * 19), Color.Indigo * 0.05f * Intensity);
+                    var planetPos = new Vector2((Main.screenWidth / 4) * 3, Main.screenHeight / 4);
+                    spriteBatch.Draw(PlanetTexture, planetPos, null, Color.White * 0.9f * this.Intensity, 0f, new Vector2(PlanetTexture.Width >> 1, PlanetTexture.Height >> 1), 1f, SpriteEffects.None, 1f);
                 }
             }
         }
