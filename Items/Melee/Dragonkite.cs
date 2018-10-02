461ca54ccedfb722306bb83af901d865b2825340
Jon Oakeson <38961891+Alphakip@users.noreply.github.com>
2018-10-02 16:43:19 -0700

    sword glows

--- Items/Melee/Dragonkite.cs
+++ Items/Melee/Dragonkite.cs
@@ -10,9 +10,27 @@ namespace AAMod.Items.Melee   //where is located
 {
     public class Dragonkite : ModItem
     {
+        public static short customGlowMask = 0;
+        public override void SetStaticDefaults()
+        {
+            DisplayName.SetDefault("Dragonkite");
+            Tooltip.SetDefault("");
+            if (Main.netMode != 2)
+            {
+                Microsoft.Xna.Framework.Graphics.Texture2D[] glowMasks = new Microsoft.Xna.Framework.Graphics.Texture2D[Main.glowMaskTexture.Length + 1];
+                for (int i = 0; i < Main.glowMaskTexture.Length; i++)
+                {
+                    glowMasks[i] = Main.glowMaskTexture[i];
+                }
+                glowMasks[glowMasks.Length - 1] = mod.GetTexture("Items/Melee/" + GetType().Name + "_Glow");
+                customGlowMask = (short)(glowMasks.Length - 1);
+                Main.glowMaskTexture = glowMasks;
+            }
+        }
         public override void SetDefaults()
         {
 
+            item.glowMask = customGlowMask;
             item.damage = 260;            //Sword damage
             item.melee = true;            //if it's melee
             item.width = 176;              //Sword width
@@ -28,11 +46,7 @@ namespace AAMod.Items.Melee   //where is located
             item.useTurn = true; 
         }
 
-    public override void SetStaticDefaults()
-    {
-      DisplayName.SetDefault("Dragonkite");
-      Tooltip.SetDefault("");
-    }
+        
 
         public override void AddRecipes()  //How to craft this sword
         {
