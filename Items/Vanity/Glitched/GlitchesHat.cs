549d6247cf66be33c9c3c91ea315e93c7da4d88b
Alphakip <neondarkrai8@gmail.com>
2018-10-03 21:58:50 -0600

    k

--- Items/Vanity/Glitched/GlitchesHat.cs
+++ Items/Vanity/Glitched/GlitchesHat.cs
@@ -5,7 +5,7 @@ using Terraria;
 using Terraria.ID;
 using Terraria.ModLoader;
 
-namespace AAMod.Items.Vanity.Glitched
+namespace CollisionMod.Items.Armor.DEV
 {
 	[AutoloadEquip(EquipType.Head)]
 	public class GlitchesHat : ModItem
@@ -13,37 +13,20 @@ namespace AAMod.Items.Vanity.Glitched
 		public override void SetStaticDefaults()
 		{
 			DisplayName.SetDefault("Gl17cH's Helmet");
-			Tooltip.SetDefault("'Great for impersonating AA devs!'");
+			Tooltip.SetDefault("Great for impersonating AA devs!");
 		}
 
 		public override void SetDefaults()
 		{
 			item.width = 20;
-			item.height = 24;
+			item.height = 22;
 			item.rare = 10;
 			item.vanity = true;
 		}
-        public override void ModifyTooltips(List<TooltipLine> list)
-        {
-            foreach (TooltipLine line2 in list)
-            {
-                if (line2.mod == "Terraria" && line2.Name == "ItemName")
-                {
-                    line2.overrideColor = new Color(247, 0, 42);
-                }
-            }
-        }
 
-        public override bool IsArmorSet(Item head, Item body, Item legs)
+		public override bool IsArmorSet(Item head, Item body, Item legs)
 		{
 			return body.type == mod.ItemType("GlitchesBreastplate") && legs.type == mod.ItemType("GlitchesGreaves");
 		}
-		
-		public override void UpdateVanitySet(Player player)
-		{
-			int dust = Dust.NewDust(new Vector2(player.position.X - 14 + (player.direction == -1f ? 40 : 0), player.position.Y - 0), 6, 6, 6, Main.rand.Next(-10,10)/5, Main.rand.Next(-10,10)/5, 200, default(Color), 2.0f);
-			Main.dust[dust].noGravity = true;
-			Main.dust[dust].noLight = true;
-        }
 	}
 }
