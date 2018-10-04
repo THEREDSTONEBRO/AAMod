8e1c62967c7b92bcebc4446ce3b93eac426dcd09
Alphakip <neondarkrai8@gmail.com>
2018-10-03 21:34:58 -0600

    k

---
+++ Items/Projectiles/Antimatter.cs
@@ -0,0 +1,48 @@
+using Microsoft.Xna.Framework;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace AAMod.Items.Projectiles
+{
+    public class Antimatter : ModProjectile
+    {
+        public override void SetDefaults()
+        {
+            projectile.width = 4;
+            projectile.height = 4;
+            projectile.aiStyle = 0;
+            projectile.hostile = false;
+            projectile.magic = true;
+            projectile.extraUpdates = 100;
+            projectile.timeLeft = 100;
+            projectile.penetrate = -1;
+            projectile.tileCollide = false;
+        }
+
+		public override void SetStaticDefaults()
+		{
+		DisplayName.SetDefault("Antimatter");
+		}
+
+        public override void AI()
+        {
+            projectile.localAI[0] += 1f;
+            if (projectile.localAI[0] > 9f)
+            {
+                for (int num447 = 0; num447 < 4; num447++)
+                {
+                    Vector2 vector33 = projectile.position;
+                    vector33 -= projectile.velocity * (num447 * 0.25f);
+                    projectile.alpha = 255;
+                    int num448 = Dust.NewDust(vector33, 1, 1, 173, 0f, 0f, 0, new Color(120, 0, 30), 1f);
+                    Main.dust[num448].position = vector33;
+                    Main.dust[num448].scale = Main.rand.Next(70, 110) * 0.013f;
+                    Main.dust[num448].velocity *= 0.2f;
+                }
+                return;
+            }
+        }
+
+    }
+}
