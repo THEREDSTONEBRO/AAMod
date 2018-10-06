b71b4cf1bd78065b0b53a8c165e771e2844611b0
Alphakip <neondarkrai8@gmail.com>
2018-10-06 16:28:39 -0600

    bg stuff

--- Backgrounds/Mire/MireUgBgStyle.cs
+++ Backgrounds/Mire/MireUgBgStyle.cs
@@ -1,18 +1,21 @@
-﻿using Terraria;
+using Terraria;
 using Terraria.ModLoader;
 
 namespace AAMod.Backgrounds.Mire
 {
-    class MireUgBgStyle : ModUgBgStyle
-    {
-        public override bool ChooseBgStyle()
-        {
-            return Main.LocalPlayer.GetModPlayer<AAPlayer>(mod).ZoneMire;
-        }
+	public class MireUgBgStyle : ModUgBgStyle
+	{
+		public override bool ChooseBgStyle()
+		{
+			return !Main.gameMenu && Main.player[Main.myPlayer].GetModPlayer<AAPlayer>(mod).ZoneMire;
+		}
 
-        public override void FillTextureArray(int[] textureSlots)
-        {
-            textureSlots[0] = mod.GetBackgroundSlot("Backgrounds/Mire/MireUnderground");
-        }
-    }
+		public override void FillTextureArray(int[] textureSlots)
+		{
+			textureSlots[0] = mod.GetBackgroundSlot("Backgrounds/Mire/MireUnderground1");
+			textureSlots[1] = mod.GetBackgroundSlot("Backgrounds/Mire/MireUnderground");
+			textureSlots[2] = mod.GetBackgroundSlot("Backgrounds/Mire/MireCavern1");
+			textureSlots[3] = mod.GetBackgroundSlot("Backgrounds/Mire/MireCavern");
+		}
+	}
 }
