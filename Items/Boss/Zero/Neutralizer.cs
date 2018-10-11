6b5dfed062b63bcf63ff4123c382683a8690a712
Alphakip <neondarkrai8@gmail.com>
2018-10-10 22:05:00 -0600

    ok

--- Items/Boss/Zero/Neutralizer.cs
+++ Items/Boss/Zero/Neutralizer.cs
@@ -6,7 +6,7 @@ using System.Collections.Generic;
 using System.Linq;
 using Microsoft.Xna.Framework.Graphics;
 
-namespace AAMod.Items.Ranged
+namespace AAMod.Items.Boss.Zero
 {
 	public class Neutralizer : ModItem
 	{
