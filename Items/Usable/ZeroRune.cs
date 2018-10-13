a5647ea8432cecfa8eaf270b96a29e9f7c8eaf44
Jon Oakeson <38961891+Alphakip@users.noreply.github.com>
2018-10-12 22:05:03 -0700

    ok

--- Items/Usable/ZeroRune.cs
+++ Items/Usable/ZeroRune.cs
@@ -7,19 +7,18 @@ using Terraria.ModLoader;
 namespace AAMod.Items.Usable
 {
     //imported from my tAPI mod because I'm lazy
-    public class ZeroTesseract : ModItem
+    public class ZeroRune : ModItem
     {
         public override void SetStaticDefaults()
         {
-            DisplayName.SetDefault("ERROR:NULL");
-            Tooltip.SetDefault(@"DESCRIPTI0NHERE
-UNSTABLE. C0NTAINS C0DE TO ACTIVATE THE BRINGER 0F DEATH");
+            DisplayName.SetDefault("0");
+            Tooltip.SetDefault(@"ACTIVATES THE GR0UND ZER0 C0DE F0R THE NEAREST ZER0 UNIT");
         }
 
         public override void SetDefaults()
         {
-            item.width = 38;
-            item.height = 38;
+            item.width = 32;
+            item.height = 32;
             item.rare = 11;
             item.value = Item.sellPrice(0, 0, 0, 0);
             item.useAnimation = 45;
@@ -42,24 +41,27 @@ UNSTABLE. C0NTAINS C0DE TO ACTIVATE THE BRINGER 0F DEATH");
         public override bool CanUseItem(Player player)
         {
             AAPlayer modPlayer = player.GetModPlayer<AAPlayer>(mod);
-            return modPlayer.ZoneVoid && !NPC.AnyNPCs(mod.NPCType("Zero")) && !NPC.AnyNPCs(mod.NPCType("ZeroAwakened"));
+            return modPlayer.ZoneVoid && !NPC.AnyNPCs(mod.NPCType("Zero")) && !NPC.AnyNPCs(mod.NPCType("ZeroAwakened")) && Main.expertMode;
         }
 
         public override bool UseItem(Player player)
         {
-            NPC.SpawnOnPlayer(player.whoAmI, mod.NPCType("Zero"));
-            Main.PlaySound(mod.GetLegacySoundSlot(SoundType.Custom, "Sounds/Sounds/Glitch"));
+            NPC.SpawnOnPlayer(player.whoAmI, mod.NPCType("ZeroAwakened"));
+            Main.PlaySound(mod.GetLegacySoundSlot(SoundType.Custom, "Sounds/Sounds/ZeroDeath"));
             return true;
         }
 
         public override void AddRecipes()
         {
+            if (Main.expertMode == true)
+            { 
             ModRecipe recipe = new ModRecipe(mod);
-            recipe.AddIngredient(null, "ApocalyptitePlate", 15);
-            recipe.AddIngredient(null, "DarkMatter", 20);
-            recipe.AddTile(null, "QuantumFusionAccelerator");
+            recipe.AddIngredient(null, "ApocalyptitePlate", 10);
+            recipe.AddIngredient(null, "UnstableSingularity", 10);
+            recipe.AddTile(null, "BinaryReassembler");
             recipe.SetResult(this, 1);
             recipe.AddRecipe();
+            }
         }
     }
 }
--- NPCs/Enemies/Void/Null.cs
+++ NPCs/Enemies/Void/Null.cs
@@ -8,46 +8,47 @@ using Terraria.ModLoader;
 
 namespace AAMod.NPCs.Enemies.Void
 {
-	public class Searcher : ModNPC
+	public class Null : ModNPC
 	{
-		public int timer = 0;
-		public bool start = true;
 		
 		public override void SetStaticDefaults()
 		{
-			DisplayName.SetDefault("Searcher");
-            Main.npcFrameCount[npc.type] = 5;
+			DisplayName.SetDefault("Null");
+            Main.npcFrameCount[npc.type] = 4;
         }
 		
 		public override void SetDefaults()
 		{
-            npc.CloneDefaults(NPCID.Probe);
-            npc.aiStyle = 5;
-			npc.width = 30;
-			npc.height = 30;
-			npc.noGravity = true;
-			npc.noTileCollide = true;
-			npc.chaseable = false;
-			npc.damage = 30;
-			npc.defense = 20;
-			npc.lifeMax = 1000;
-			npc.HitSound = SoundID.NPCHit4;
-			npc.DeathSound = SoundID.NPCDeath14;
-		}
+            npc.CloneDefaults(NPCID.Ghost);
+            animationType = NPCID.Ghost;
+            npc.noGravity = true;
+            npc.noTileCollide = true;
+            npc.width = 24;
+            npc.height = 40;
+            npc.aiStyle = 36;
+            npc.damage = 50;
+            npc.defense = 9999999;
+            npc.lifeMax = 100;
+            npc.HitSound = SoundID.NPCHit1;
+            npc.DeathSound = SoundID.NPCDeath6;
+            npc.alpha = 70;
+            npc.value = 7000f;
+            npc.knockBackResist = 0.7f;
+        }
         public override float SpawnChance(NPCSpawnInfo spawnInfo)
         {
             Player player = spawnInfo.player;
             if (!(player.ZoneTowerSolar || player.ZoneTowerVortex || player.ZoneTowerNebula || player.ZoneTowerStardust) && ((!Main.pumpkinMoon && !Main.snowMoon) || spawnInfo.spawnTileY > Main.worldSurface || Main.dayTime) && (!Main.eclipse || spawnInfo.spawnTileY > Main.worldSurface || !Main.dayTime) && (SpawnCondition.GoblinArmy.Chance == 0))
             {
                 int[] TileArray2 = { mod.TileType("Doomstone"), mod.TileType("Apocalyptite"), mod.TileType("DoomstoneBrick") };
-                return TileArray2.Contains(Main.tile[spawnInfo.spawnTileX, spawnInfo.spawnTileY].type) && NPC.downedMoonlord ? 6.09f : 0f;
+                return TileArray2.Contains(Main.tile[spawnInfo.spawnTileX, spawnInfo.spawnTileY].type) && AAWorld.downedZero ? 6.09f : 0f;
             }
             return 0f;
         }
 
         public override void NPCLoot()
         {
-            Item.NewItem((int)npc.position.X, (int)npc.position.Y, npc.width, npc.height, mod.ItemType("Apocalyptite"), 1);
+            Item.NewItem((int)npc.position.X, (int)npc.position.Y, npc.width, npc.height, mod.ItemType("UnstableSingularity"), 1);
         }
     }
 }
